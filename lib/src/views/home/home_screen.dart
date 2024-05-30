f7279c1a4ddbe2afe39046a9ad80dd1d42a32493
muticansel <cancel.muti@gmail.com>
2024-05-30 23:26:42 +0300

    feat: add Exit button on Home screen

--- lib/src/views/home/home_screen.dart
+++ lib/src/views/home/home_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:crossfit_app/src/views/login/login_screen.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+                (Route<dynamic> route) => false,
+              );
+            },
+            child: const Text(
+              'Çıkış Yap',
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
         child: Text('Home'),
       ),
     );
--- lib/src/views/login/login_screen.dart
+++ lib/src/views/login/login_screen.dart
@@ -7,77 +7,79 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(fit: StackFit.expand, children: <Widget>[
-      Opacity(
-        opacity: 0.5,
-        child: Image.asset(
-          'assets/login_background.jpg',
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Opacity(
+          opacity: 0.5,
+          child: Image.asset(
+            'assets/login_background.jpg',
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      Container(
-        color: Colors.black.withOpacity(
-            0.6), // Arka plan resminin üstüne daha az opak yarı saydam bir katman ekler
-      ),
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Telefon Numarası',
+        Container(
+          color: Colors.black.withOpacity(
+              0.6), // Arka plan resminin üstüne daha az opak yarı saydam bir katman ekler
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Telefon Numarası',
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Şifre',
+              const SizedBox(height: 16),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Şifre',
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                const Row(
-                  children: <Widget>[
-                    SizedBox(width: 4),
-                    CustomCheckbox(),
-                    SizedBox(width: 8),
-                    Text(
-                      'Beni Hatırla',
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Row(
+                    children: <Widget>[
+                      SizedBox(width: 4),
+                      CustomCheckbox(),
+                      SizedBox(width: 8),
+                      Text(
+                        'Beni Hatırla',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Şifremi Unuttum butonuna basılınca yapılacak işlemler
+                    },
+                    child: const Text(
+                      'Şifremi Unuttum',
                       style: TextStyle(color: Colors.white),
                     ),
-                  ],
-                ),
-                TextButton(
-                  onPressed: () {
-                    // Şifremi Unuttum butonuna basılınca yapılacak işlemler
-                  },
-                  child: const Text(
-                    'Şifremi Unuttum',
-                    style: TextStyle(color: Colors.white),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Home()),
-                );
-              },
-              child: const Text('Giriş Yap'),
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Home()),
+                  );
+                },
+                child: const Text('Giriş Yap'),
+              ),
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
